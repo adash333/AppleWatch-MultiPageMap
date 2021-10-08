5b660cc48912deea9c82dcb677fa26a3b41b8ec4
“adash333” <adash333@hotmail.com>
2021-10-08 20:48:33 +0900

    Initial Commit

---
+++ MultiPageMap WatchKit Extension/MultiPageMapApp.swift
@@ -0,0 +1,19 @@
+//
+//  MultiPageMapApp.swift
+//  MultiPageMap WatchKit Extension
+//
+//  Created by a on 2021/10/08.
+//
+
+import SwiftUI
+
+@main
+struct MultiPageMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
